70cfec165cc7f4be8135afcac41ac2d9873110d2
Kapil Gupta <kapilguptansit@gmail.com>
2021-05-30 15:26:54 +0530

    javascript file

---
+++ shared.js
@@ -0,0 +1,40 @@
+const backdrop = document.querySelector('.backdrop');
+const modal = document.querySelector('.modal');
+const selectPlanButtons = document.querySelectorAll('.plan button');
+const closeModalButton = document.querySelector('.modal__action--negative');
+const toggleButton = document.querySelector('.toggle-button');
+const mobileNav = document.querySelector('.mobile-nav');
+
+
+if (selectPlanButtons) {
+    for(let i=0; i< selectPlanButtons.length; i++ ) {
+        selectPlanButtons[i].addEventListener('click', function() {
+            // modal.style.display = 'block';
+            // backdrop.style.display = 'block';
+            modal.classList.add('open');
+            backdrop.classList.add('open');
+        })
+    }
+}
+
+let closeModal = function() {
+    if (modal) {
+        modal.classList.remove('open');
+    }
+    backdrop.classList.remove('open');
+};
+
+if (closeModalButton) {
+    closeModalButton.addEventListener('click', closeModal);
+}
+
+backdrop.addEventListener('click', function() {
+    mobileNav.classList.remove('open');
+    closeModal();
+});
+
+
+toggleButton.addEventListener('click', function() {
+    mobileNav.classList.add('open');
+    backdrop.classList.add('open');
+})
